2ea780f51c73a8b82d7d31375260cf7833f9def8
Wahid077 <72840089+wahid7077@users.noreply.github.com>
2022-06-09 05:28:09 +0700

    Add files via upload

---
+++ Final Project Apss E-Parking/PLMS_Project/ParkingLotManagementSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParkingLotManagementSystem
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); ///Launches the "Form1" GUI first when running the program
+        }
+    }
+}
